362dac3d6f7ec2b520f1d9d6ac65a628f133a301
DevilMayCry6 <1061853349@qq.com>
2019-11-11 23:49:40 +0800

    使用HBuilderX编辑器进行免终端开发vue应用

---
+++ test/pages/index/index.vue
@@ -0,0 +1,23 @@
+<template>
+	<div clas="box">能看见吗</div>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style lang="less">
+	@import "../../less.less"
+</style>
