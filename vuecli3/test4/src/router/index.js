e045c61a90bf78757cde0898fa4aa80dca4981f4
DevilMayCry6 <1061853349@qq.com>
2019-11-24 23:47:07 +0800

    页面初始化及banner图片显示

--- vuecli3/test4/src/router/index.js
+++ vuecli3/test4/src/router/index.js
@@ -1,14 +1,16 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import First from '../views/First.vue'
+
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: Home
+    // name: 'home',
+    component: First
   },
   {
     path: '/about',
---
+++ vuecli3/test4/src/views/First.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="main">
+        <div class="left"></div>
+        <div>
+            <div class="top">
+                <img src="../../src/assets/logo.png" alt="">
+            </div>
+            <div class="buttom"></div>
+        </div>
+    </div>
+</template>
