5ae526f4293cc420752d7032d7ec09b2d25936a9
DevilMayCry6 <1061853349@qq.com>
2019-11-25 23:46:47 +0800

    内容设置

---
+++ vuecli3/test4/src/components/Left.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <div class="title">title</div>
+        <ul class="menu">
+            <li>左</li>
+            <li>右</li>
+            <li>左</li>
+            <li>右</li>
+            <li>左</li>
+            <li>右</li>
+            <li>左</li>
+            <li>右</li>
+            <li>左</li>
+            <li>右</li>
+            <li>左</li>
+            <li>右</li>
+        </ul>
+    </div>
+</template>
+<style>
+    .title{
+        width: 100px;
+        color: red;
+    }
+    .menu li{
+        list-style: none;
+        height: 50px;
+        margin-bottom: 2px;
+        background:white;
+        line-height: 50px;
+    }
+</style>
